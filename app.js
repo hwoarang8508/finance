e11731d13eec6b02a741a7c969a5b4b07053e83f
ks0826 <hwoarang8508@naver.com>
2023-01-10 23:51:52 +0800

    huwisagchdgiig daldlaw

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 //дэлгэцтэй ажиллах контроллер
 var uiController= (function(){
 
+        var DOMstrings = {
+            inputType: ".add__type",
+            inputDescription:  ".add__description",
+            inputValue: ".add__value",
+            addBtn: ".add__btn"
+        };
+        return {
+                getInput: function ()
+                        {
+                            return{
+                                type: document.querySelector(DOMstrings.inputType).value,
+                                description: document.querySelector(DOMstrings.inputDescription).value,
+                                value: document.querySelector(DOMstrings.inputValue).value
+                            };
+                        },
+                        getDOMstrings: function(){
+                            return DOMstrings;
+                        }
+                };
 })();
 
 //санхүүтэй ажиллах контроллер
 
-var financeController = (function(){
-
-})();
+var financeController = (function(){})();
 
 //программын холбогч контроллер
 
-var appController= (function(uiController,financeController){
+var appController= function(uiController,financeController){
+    var DOM = uiController.getDOMstrings();
 
-    var ctrlAddItem = function(){
-    console.log('clicked...')
-    }
- document.querySelector('.add__btn').addEventListener('click',function(){
-   ctrlAddItem();
- })
+    var ctrlAddItem = function(){console.log(uiController.getInput())}
+    document.querySelector(DOM.addBtn).addEventListener('click',function(){ctrlAddItem();
+ });
 document.addEventListener("keypress",function(event) {
     if(event.keyCode === 13|| event.which ===13) {ctrlAddItem()};
 });
-
-
-})(uiController,financeController);
+}(uiController,financeController);
