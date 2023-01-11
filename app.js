037e9637331ce3f65101f1cbf78d25188de32247
ks0826 <hwoarang8508@naver.com>
2023-01-11 23:03:20 +0800

    income expense object uusgew

--- app.js
+++ app.js
@@ -1,40 +1,78 @@
 //дэлгэцтэй ажиллах контроллер
 var uiController= (function(){
-
-        var DOMstrings = {
-            inputType: ".add__type",
-            inputDescription:  ".add__description",
-            inputValue: ".add__value",
-            addBtn: ".add__btn"
-        };
-        return {
-                getInput: function ()
-                        {
-                            return{
-                                type: document.querySelector(DOMstrings.inputType).value,
-                                description: document.querySelector(DOMstrings.inputDescription).value,
-                                value: document.querySelector(DOMstrings.inputValue).value
-                            };
-                        },
-                        getDOMstrings: function(){
-                            return DOMstrings;
-                        }
-                };
-})();
+                                var DOMstrings = {
+                                    inputType: ".add__type",
+                                    inputDescription:  ".add__description",
+                                    inputValue: ".add__value",
+                                    addBtn: ".add__btn"
+                                };
+                                return {
+                                        getInput: function()
+                                                {
+                                                    return{
+                                                        type: document.querySelector(DOMstrings.inputType).value,
+                                                        description: document.querySelector(DOMstrings.inputDescription).value,
+                                                        value: document.querySelector(DOMstrings.inputValue).value
+                                                    };
+                                                },
+                                                getDOMstrings: function()
+                                                {
+                                                    return DOMstrings;
+                                                }
+                                        };
+                            })();
 
 //санхүүтэй ажиллах контроллер
 
-var financeController = (function(){})();
+var financeController = (function(){
+
+var Income = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+
+var Expense = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+
+var data = {
+    allItems: {
+        inc: [],
+        exp: []
+    },
+     totals: {
+        inc:200,
+        exp:500
+     }
+}
+
+
+})();
 
 //программын холбогч контроллер
 
-var appController= function(uiController,financeController){
+var appController= (function(uiController,financeController){
     var DOM = uiController.getDOMstrings();
+    var ctrlAddItem = function(){console.log(uiController.getInput())};
 
-    var ctrlAddItem = function(){console.log(uiController.getInput())}
-    document.querySelector(DOM.addBtn).addEventListener('click',function(){ctrlAddItem();
- });
-document.addEventListener("keypress",function(event) {
-    if(event.keyCode === 13|| event.which ===13) {ctrlAddItem()};
-});
-}(uiController,financeController);
+    var setupEventListeners = function(){
+        var DOM = uiController.getDOMstrings();
+        document.querySelector(DOM.addBtn).addEventListener('click',function(){ctrlAddItem()
+        });
+        document.addEventListener("keypress",function(event) {
+                if(event.keyCode === 13|| event.which ===13) {
+                    ctrlAddItem();
+                }
+            });
+        };
+        return {
+            init: function(){
+                console.log("programm started");
+                setupEventListeners();
+            }
+        }
+    })(uiController,financeController);
+    appController.init();
